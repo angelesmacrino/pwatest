0fa8869bf202d4258b14b840ecb7230c392e2b04
angeles <carolinamacrino@gmail.com>
2024-03-01 22:22:13 -0300

    created dynamic caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const STATIC_CACHE_NAME = 'site-static';
+const DYNAMIC_CACHE_NAME = 'site-dynamic';
 const ASSETS = [
     //requests
     '/',
@@ -23,6 +24,15 @@ function cacheApp() {
 
 self.addEventListener('activate', evt => {
     //console.log('Service Worker has been activated');
+    //delete old caches, loop and delete every cache that is not current cache
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== STATIC_CACHE_NAME)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
 //fetch event
@@ -32,7 +42,13 @@ self.addEventListener('fetch', evt => {
     evt.respondWith(
         caches.match(evt.request)
         .then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                //stores the request in the dynamic cache
+                return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                });
+            });
         })
     );
 });
