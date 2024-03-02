6e5c6a39e1c6246c354824358b9dbaf400a8097a
angeles <carolinamacrino@gmail.com>
2024-03-01 21:39:55 -0300

    added caching on sw

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
-console.log('ih')
-
+const STATIC_CACHE_NAME = 'site-static';
+const ASSETS = [
+    //requests
+    '/',
+    '/index.html',
+    //scripts
+    '/js/app.js',
+    '/js/submit.js'
+];
+//aca cacheo cosas q no cambien muy seguido
 self.addEventListener('install', evt => {
-    console.log('Service Worker has been installed');
+    //no termina el evento hasta que no se complete la promesa del cache
+    evt.waitUntil(cacheApp());
 });
 
+function cacheApp() {
+    caches.open(STATIC_CACHE_NAME)
+    .then(cache => {
+        console.log('caching shell assets');
+        cache.addAll(ASSETS);
+    });
+}
+
 self.addEventListener('activate', evt => {
-    console.log('Service Worker has been activated');
+    //console.log('Service Worker has been activated');
 });
 
 //fetch event
 
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    //me fjo si el request esta en el cache, si no lo agrego
+    evt.respondWith(
+        caches.match(evt.request)
+        .then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
